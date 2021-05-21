d83be7661a78a66b9f29fb75276d589dc6a7bf18
masaya-kawamura <lucky.number0128@gmail.com>
2021-05-21 11:16:46 +0000

    [Create]jpostl

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,19 @@ class User < ApplicationRecord
     has_many :books, dependent: :destroy
 
     attachment :profile_image
+    
+    # 都道府県コードから当道府県名に自動で変換する。
+    include JpPrefecture
+    jp_prefecture :prefecture_code
+    
+    # ~.prefecture_nameで都道府県名を参照できるようにする。
+    # @user.prefecture_nameで該当ユーザーの住所（都道府県）を表示できる。
+    def prefecture_name
+      JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+    end
+    
+    def prefecture_name=(prefecture_name)
+      self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+    end
+    
 end
